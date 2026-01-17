d8ae172974baefe30ddee9c8a3e7929e1a03d879
MAULESH DEV <163156946+catxy07@users.noreply.github.com>
2026-01-17 19:19:39 +0530

    Add Location model with geolocation fields

---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Float, TIMESTAMP, text
+from db import Base
+
+class Location(Base):
+    __tablename__ = "locations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String(50), nullable=False, index=True)
+
+    lat = Column(Float, nullable=False)
+    lng = Column(Float, nullable=False)
+
+    ip_address = Column(String(45), nullable=True)  # supports IPv4 + IPv6
+
+    time = Column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP"))
